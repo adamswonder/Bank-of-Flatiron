7b50acc89dd37e6297403de27acda8b45661591e
moringawonder <adams.wonderboy@student.moringaschool.com>
2022-09-30 20:23:35 +0300

    adds mapped transactions

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,10 +1,11 @@
 import React from "react";
 
 function Transaction(props) {
-  // initializes a new array and maps each transaction to respective table fields.
+  // initializes a new array and maps each transaction to respective table field forms.
   const mappedTransactions = props.transactions.map((transaction) => {
 
     return (
+      // table data gets displayed here
       <tr>
         <td>{transaction.date}</td>
         <td>{transaction.description}</td>
